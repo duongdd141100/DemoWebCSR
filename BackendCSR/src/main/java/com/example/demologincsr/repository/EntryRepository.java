4e2dbfbbf1d8a7cd09f1dd7fc317ea2847b1c13f
Do Duc Duong <duongdd2@vingroup.net>
2023-03-24 12:07:27 +0700

    [BE] Update get entries

--- BackendCSR/src/main/java/com/example/demologincsr/repository/EntryRepository.java
+++ BackendCSR/src/main/java/com/example/demologincsr/repository/EntryRepository.java
@@ -4,6 +4,11 @@ import com.example.demologincsr.entity.Entry;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EntryRepository extends JpaRepository<Entry, Long> {
+
+    List<Entry> findAllByPosition(String position);
+
 }
