75249f0f307bde8dc525b894537d81af7a14a0a1
Do Duc Duong <duongdd2@vingroup.net>
2023-03-24 09:44:28 +0700

    [BE] Create enum for error message

--- BackendCSR/src/main/java/com/example/demologincsr/common/ErrorMessageEnum.java
+++ BackendCSR/src/main/java/com/example/demologincsr/common/ErrorMessageEnum.java
@@ -1,2 +1,30 @@
-package com.example.demologincsr.common;public enum ErrorMessageEnum {
+package com.example.demologincsr.common;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorMessageEnum {
+    LOGIN_FAILED("001001", "Login Failed!"),
+
+    TOKEN_EMPTY("001002", "Token Empty!"),
+
+    TOKEN_INVALID("001003", "Token Invalid!");
+
+    private String code;
+
+    private String message;
+
+    ErrorMessageEnum(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public static ErrorMessageEnum typeOf(String code) {
+        for (ErrorMessageEnum messageEnum : ErrorMessageEnum.values()) {
+            if (messageEnum.code.equals(code)) {
+                return messageEnum;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
 }
