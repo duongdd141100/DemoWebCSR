12eee5678cdd86eb4a40f11aa36248214264c706
Do Duc Duong <duongdd2@vingroup.net>
2023-03-24 10:09:40 +0700

    [BE] Update enum of error message

--- BackendCSR/src/main/java/com/example/demologincsr/common/ErrorMessageEnum.java
+++ BackendCSR/src/main/java/com/example/demologincsr/common/ErrorMessageEnum.java
@@ -4,11 +4,13 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorMessageEnum {
-    LOGIN_FAILED("001001", "Login Failed!"),
+    LOGIN_FAILED("001001", "Login Failed"),
 
-    TOKEN_EMPTY("001002", "Token Empty!"),
+    TOKEN_EMPTY("001002", "Token Empty"),
 
-    TOKEN_INVALID("001003", "Token Invalid!");
+    TOKEN_INVALID("001003", "Token Invalid"),
+
+    CREATE_TOKEN_FAILED("001004", "Create Token Failed");
 
     private String code;
 
