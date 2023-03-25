bca3fba8e481de839d1026ace47ff20d2df56fda
Do Duc Duong <duongdd2@vingroup.net>
2023-03-25 14:37:42 +0700

    [FE] Remove commentout code

--- frontend-csr/src/components/Login.js
+++ frontend-csr/src/components/Login.js
@@ -2,40 +2,6 @@ import { useState } from "react"
 import { signIn } from "../constants/ServiceConstants";
 import { post } from "../services/Service"
 
-function loginHandler(user, pass, setToken) {
-    post(signIn,
-        JSON.stringify({
-            username: user,
-            password: pass
-        }),
-        (data) => setToken(data.body),
-        (data) => {throw new Error(data.body);}
-        );
-    // fetch(signIn,
-    //     {
-    //         method: 'POST',
-    //         body: JSON.stringify({
-    //             username: user,
-    //             password: pass
-    //         })
-    //     })
-    //     .then(response => {
-    //         if (!response.ok) {
-    //             return response.json();
-    //         }
-    //         let object = response.json().then((result) => result)
-    //         console.log(object + 1)
-    //         throw new Error(response.json().then((result) => result.body))
-    //     })
-    //     .then(data => {
-    //         throw new Error(data.body);
-    //         setToken(`Bearer ${data.body}`)
-    //     })
-    //     .catch(error => {
-    //         console.log(error.message);
-    //     })
-}
-
 export default function Login({ setToken }) {
     const [user, setUser] = useState("");
     const [pass, setPass] = useState("");
