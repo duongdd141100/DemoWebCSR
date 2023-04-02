5b704a995b4e2269b9443014c6726c4db483f109
Đỗ Đức Dương <83938501+duongdd141100@users.noreply.github.com>
2023-04-02 15:04:45 +0700

    [FE] CSS for login

--- frontend-csr/src/components/Login.js
+++ frontend-csr/src/components/Login.js
@@ -1,18 +1,33 @@
 import { useState } from "react"
 import { signIn } from "../constants/ServiceConstants";
 import { post } from "../services/Service"
+import '../css/Common.css'
+
+const divStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+    flexDirection: 'column',
+}
 
 export default function Login({ setToken }) {
     const [user, setUser] = useState("");
     const [pass, setPass] = useState("");
-    return <div>
-        <label>username</label>
-        <input type="text" name="username" id="username" onChange={e => setUser(e.target.value)}/>
+    return <div style={divStyle}>
+        <div>
+            <h3>Demo Web</h3>
+        </div>
+        <div>
+            <label>Username: </label>
+            <input type="text" name="username" id="username" onChange={e => setUser(e.target.value)}/>
+        </div>
+        <div>
+            <label>Password: </label>
+            <input type={"password"} name="password" id="password" onChange={e => setPass(e.target.value)} />
+        </div>
 
-        <label>password</label>
-        <input type={"password"} name="password" id="password" onChange={e => setPass(e.target.value)} />
-
-        <button onClick={() => post(signIn,
+        <button className="successButton" onClick={() => post(signIn,
             JSON.stringify({
                 username: user,
                 password: pass
