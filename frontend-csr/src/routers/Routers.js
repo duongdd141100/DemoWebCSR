00097444129f892fb3bf4b1d3c253dc77ab5e591
Đỗ Đức Dương <83938501+duongdd141100@users.noreply.github.com>
2023-04-06 09:26:42 +0700

    [FE] Update Routers

--- frontend-csr/src/routers/Routers.js
+++ frontend-csr/src/routers/Routers.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import Login from '../components/Login'
 import MainLayout from '../layouts/MainLayout'
-import { getHeaderApi } from '../constants/ServiceConstants'
+import { getHeaderApi, getMenuApi } from '../constants/ServiceConstants'
 import { Services } from '../services/Service'
 import Home from '../layouts/Home'
 import About from '../layouts/About'
@@ -34,10 +34,15 @@ let menuRouters = {
 export default function Routers() {
     const [token, setToken] = useState("");
     const [header, setHeader] = useState([]);
+    const [menu, setMenu] = useState([])
     // localStorage.setItem("token", "")
     useEffect(() => {
         if (Object.keys(token).length !== 0 || localStorage.getItem("token").length !== 0) {
             Services.get(getHeaderApi, (data) => setHeader(data.body), (data) => {
+                throw new Error(data.body);
+            }, token)
+
+            Services.get(getMenuApi, (data) => setMenu(data.body), (data) => {
                 throw new Error(data.body);
             }, token)
         }
@@ -46,14 +51,21 @@ export default function Routers() {
     if (Object.keys(token).length === 0 && localStorage.getItem("token").length === 0) {
         return <Login setToken={(value) => setToken(value)}/>
     }
-    header.forEach(r => {
-        headerRouters[r.slug].label = r.label;
+    header.forEach(x => {
+        headerRouters[x.slug].label = x.label;
     });
+    menu.forEach(x => {
+        menuRouters[x.slug].label = x.label;
+        menuRouters[x.slug].parent = x.parent;
+    })
     return (
-        <MainLayout header={headerRouters} menu={menuRouters}>
+        <MainLayout header={headerRouters} menuRouters={menuRouters} menu={menu}>
             <Routes>
                 {Object.keys(headerRouters).map(key => {
                     return <Route key={key} path={key} element={headerRouters[key].component} />
+                })}
+                {Object.keys(menuRouters).map(key => {
+                    return <Route key={key} path={key} element={menuRouters[key].component} />
                 })}
             </Routes>
         </MainLayout>
